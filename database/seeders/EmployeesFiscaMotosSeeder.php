d32301b2858a8aad9b465c46f0debc3a921cef70
RyouhaWH <ia.kaziell@gmail.com>
2025-07-15 16:51:10 -0400

    carga empleados

---
+++ database/seeders/EmployeesFiscaMotosSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rol;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeesFiscaMotosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $rolFisca = Rol::firstOrCreate(['nombre' => 'Fiscalizacion']);
+
+        $rolMoto = Rol::firstOrCreate(['nombre' => 'Motorizado']);
+
+        $PersonalMotorizado = [
+            "Cesar Soto",
+            "Leonardo Sanhueza",
+            "Fernando Maturana",
+            "Roman Lepin",
+            "Nicolas Correa",
+            "Joan Ramirez",
+            "Raul Carvajal",
+            "Pablo Riquelme",
+            "Freddy Gonzalez",
+            "Christofer Gonzalez",
+        ];
+
+        $personalFiscalización = [
+            "Elias Fuentes",
+            "Javier Fernandez",
+            "Victor Silva",
+            "Jonathan Torres",
+            "Matias Caniulef",
+            "Natalia Ñanco",
+            "Kevin Vizcarra",
+            "Uriel Toro Toro",
+            "Raul Huaiquinao Traipe",
+            "Robinson Oyarzun",
+            "Bernardino Tropa",
+            "Fabiola Novoa",
+            "Wilson Vidal Paredes",
+            "Daniel Obreque",
+            "Andrés Painen",
+            "Sara Paredes",
+            "Raul Campos Ortega",
+            "Alvaro Neira",
+            "Juan Becerra",
+            "Danny Choquechambe",
+            "Eduardo Olivares",
+            "Rodrigo Ponce",
+            "Gastón Salazar",
+        ];
+
+
+    }
+}
