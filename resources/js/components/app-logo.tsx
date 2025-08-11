1b0e5ae37547095131a3e17dc1502aa822fb666e
RyouhaWH <ia.kaziell@gmail.com>
2025-08-11 13:14:30 -0400

    terminado app de turnos, faltan roles

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,13 +2,13 @@ import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
-        <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+        <div className='flex flex-row items-center bg-white rounded-md w-full'>
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground ">
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+            <div className="ml-1 grid flex-1 text-left text-sm pl-2">
+                <img className='h-10 pt-1' src="/banner-amzoma.png" alt="logo" />
             </div>
-        </>
+        </div>
     );
 }
