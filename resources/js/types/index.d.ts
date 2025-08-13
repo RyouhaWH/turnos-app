567295f27e60aa0b41543ffdd4f7cffc374d0a47
RyouhaWH <ia.kaziell@gmail.com>
2025-08-13 14:57:18 -0400

    fix: Add roles property to User type definition

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -39,5 +39,9 @@ export interface User {
     email_verified_at: string | null;
     created_at: string;
     updated_at: string;
+    roles?: Array<{
+        id: number;
+        name: string;
+    }>;
     [key: string]: unknown; // This allows for additional properties...
 }
